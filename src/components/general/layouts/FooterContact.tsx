a187f51bf56b7cfe9e8f4c04330ba028b79be6e4
Ameh Abraham <amehabraham811@gmail.com>
2024-10-09 16:32:42 +0100

    worked on netlify error

--- src/components/general/layouts/FooterContact.tsx
+++ src/components/general/layouts/FooterContact.tsx
@@ -6,7 +6,7 @@ const FooterContact: FC = () => {
     <div className="text-gray-600 basis-1/2 sm:mt-5 lg:mt-0">
       <div className="py-2 flex flex-col text-center gap-0">
         <p className="text-left text-3xl sm:text-4xl lg:text-5xl font-sans text-secondary">
-          <span className="text-black font-bold font-serif">Let's </span>
+          <span className="text-black font-bold font-serif">Let&apos;s </span>
           Connect. 💬
         </p>
         <SocialIcons />
